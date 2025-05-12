d7c63472b2f28d7663a7c0e7a348488ee294e1f2
Willington Juma <willingtonejumamconyango@gmail.com>
2025-05-12 17:44:05 +0300

    db add

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,20 @@ db=SQLAlchemy()
 
 def create_app():
     app=Flask(__name__)
+    #database cnnections 
+    app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///data.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    db.init_app(app)
 
     from .books import books_bp
     from .user import user_Bp
     
     app.register_blueprint(books_bp)
     app.register_blueprint(user_Bp)
-
+    # create tables 
+    with app.app_context():
+        from.models import Books
+        db.create_all()
 
     return app
+
