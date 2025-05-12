dbd48b48e036b3d5354e03640101ea8eddf31647
Willington Juma <willingtonejumamconyango@gmail.com>
2025-05-12 18:00:00 +0300

    .

--- app/models.py
+++ app/models.py
@@ -4,7 +4,11 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     author = db.Column(db.String(120), nullable=False)
+def to_dict(self):
+    return {"id":self.id,"title":self.title,"author":self.author}
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)    
+    
