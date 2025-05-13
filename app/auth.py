0ae4ab0e5a768c35988b4bc26fb0b783e626a7e1
Willington Juma <willingtonejumamconyango@gmail.com>
2025-05-13 21:59:10 +0300

    .

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,7 @@
 from flask import Flask ,request,jsonify,Blueprint
 from .import db,bcrypt
 from.models import User
-
+from flask_jwt_extended import create_access_token,jwt_required,get_jwt_identity
 
 
 auth_Bp=Blueprint("auth",__name__,url_prefix="getauth")
