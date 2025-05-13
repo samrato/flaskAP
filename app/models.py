c55d81dc0ea5cc7405b0792ae35a6e437340d648
Willington Juma <willingtonejumamconyango@gmail.com>
2025-05-13 16:17:01 +0300

    .

--- app/models.py
+++ app/models.py
@@ -11,5 +11,5 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)    
-def to_dict(self):
-    return{"id":self.id,"username":self.username,"email":self.email}    
+    def to_dict(self):
+           return{"id":self.id,"username":self.username,"email":self.email}    
--- app/user.py
+++ app/user.py
@@ -1,10 +1,24 @@
 from flask import Flask,jsonify,Blueprint,request
+from.models import User
+from.import db
+user_Bp=Blueprint("user",__name__,url_prefix="/getuser")
 
-user_Bp=Blueprint("user",__name__,url_prefix="/user/juma")
-
-@user_Bp.route("/")
-def list_user():
-    return jsonify({
-        "name":"kamau",
-        "age":"16"
-    })
+@user_Bp.route("/user" ,methods=['GET'])
+def Get_user():
+    try:
+        
+        user=User.query.all()
+        if not user:
+            return jsonify({"message":"There is no user availabe"}),422
+        return jsonify(u.to_dict() for u in user )
+    except Exception as e:
+        return jsonify({"message":"internam server error","details":str(e)})
+    
+@user_Bp("/add",methods=['POST'])
+def add_user():
+    try:
+     data=request.json()
+     
+  
+    except Exception as e:
+        return jsonify({"message":"internal server error","details":str(e)})
