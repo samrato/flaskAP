cabdcbbc158963365c0ab6fd3108e8e0ad3f8e9e
Willington Juma <willingtonejumamconyango@gmail.com>
2025-05-13 23:32:18 +0300

    .

--- app/auth.py
+++ app/auth.py
@@ -3,7 +3,7 @@ from .import db,bcrypt
 from.models import User
 from flask_jwt_extended import create_access_token,jwt_required,get_jwt_identity
 
-auth_Bp=Blueprint("auth",__name__,url_prefix="getauth")
+auth_Bp=Blueprint("auth",__name__,url_prefix="autheticate")
 
 @auth_Bp.route('/register',methods=['POST'])
 def register_user():
