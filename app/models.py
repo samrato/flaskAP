a64ceb62095fe50f226f4e29f205670b0aa12d1e
Willington Juma <willingtonejumamconyango@gmail.com>
2025-05-13 20:56:38 +0300

    .

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class Book(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)    
+    email = db.Column(db.String(100), unique=True, nullable=False)  
+    password = db.Column(db.String(128), nullable=False)  
     def to_dict(self):
            return{"id":self.id,"username":self.username,"email":self.email}    
