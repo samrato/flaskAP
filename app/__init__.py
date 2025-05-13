272f97c1cea9ad963eb9bc3d47ec6fabf3b8661f
Willington Juma <willingtonejumamconyango@gmail.com>
2025-05-13 20:30:20 +0300

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 db=SQLAlchemy()
+bcrypt=Bcrypt()
+jwt=JWTManager()
 
 def create_app():
     app=Flask(__name__)
