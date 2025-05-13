f07da082af8d73ba09d2943e00c0ee5ed07bc788
Willington Juma <willingtonejumamconyango@gmail.com>
2025-05-13 22:46:02 +0300

    .

--- app/auth.py
+++ app/auth.py
@@ -4,4 +4,51 @@ from.models import User
 from flask_jwt_extended import create_access_token,jwt_required,get_jwt_identity
 
 
+
+
+
+
+
+
+    # new_user = User(username=username, email=email, password=hashed_password, profile_image=profile_image)
+
+    # db.session.add(new_user)
+    # db.session.commit()
+
+    # access_token = create_access_token(identity=new_user.id)
+
+    # return jsonify({
+    #     "token": access_token,
+    #     "user": {
+    #         "userId": new_user.id,
+    #         "username": new_user.username,
+    #         "email": new_user.email,
+    #         "profileImage": new_user.profile_image,
+    #         "createdAt": new_user.created_at.isoformat()
+    #     }
+    # }), 201
+
+
 auth_Bp=Blueprint("auth",__name__,url_prefix="getauth")
+
+@auth_Bp.route('/register',methods=['POST'])
+def register_user():
+    try:
+        data=request.get_json()
+        username = data.get('username', '').strip()
+        email = data.get('email', '').strip().lower()
+        password = data.get('password', '').strip()  
+        if not username or not email or not password:
+            return jsonify({"message":"Fill in all field"}),422
+        if len(username)>3:
+            return jsonify({"message":"User name its too short"}),422
+        if len(password)>6:
+            return jsonify({"message":"Password should greater than 6 characters"})
+        existing_user=User.query.filter_by(email=email).first()
+        if existing_user:
+            return jsonify({"message":"User already exist"}),422
+        hashed_password=bcrypt.generate_password_hash(password).decode('utf-8') 
+        new_user=User(username=username,email=email,password=hashed_password)
+        db.session.add(new_user)
+    except Exception as e:
+        return jsonify({"message":"Internal server error","details":str(e)})    
